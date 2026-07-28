f47f662a927e7c37dce303362beeb35dc64b6caf
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-07-28 22:15:41 +0100

    add arrays and objects

---
+++ 13-object.js
@@ -0,0 +1,12 @@
+// Creating an object using the object literal
+const worker = {
+    name: "Maxwell Williams",
+    age: 25
+}
+
+// Using the object constructor
+const student = new Object();
+student.name = "john doe";
+student.course = "computer engineering"
+
+console.log(student);
